c754c5ab92237736fc5f57fd79b5c278d75ae26d
egorman44 <egorushka44@gmail.com>
2023-11-15 17:47:00 +0400

    [COCO] Add a packet class.

---
+++ packet.py
@@ -0,0 +1,102 @@
+import random        
+class Packet:
+
+    dbg_words_in_line = 8
+    dbg_fill = '0'
+    dbg_width = 5
+    
+    # Class members:
+    #   self.data
+    #   self.word_size
+    #   self.delay
+    
+    def __init__(self, word_size = 1):
+        self.word_size    = word_size
+        self.data         = []
+        self.format_width = int(self.word_size)*2
+
+    def compare(self, comp_pkt):
+        if len(self.data) != len(comp_pkt.data):
+            print("[Warning] Packets length are not matched")
+            return 1
+        if self.data != comp_pkt.data:
+            for indx in range(0,len(self.data)):
+                if(self.data[indx] != comp_pkt.data[indx]):
+                    print(f"[Warning] Words pkt0[{indx}] != pkt1[{indx}] ")
+                    print(f" \t pkt0[{indx}] = 0x{self.data[indx]:{Packet.dbg_fill}{self.format_width}x}")
+                    print(f" \t pkt1[{indx}] = 0x{comp_pkt.data[indx]:{Packet.dbg_fill}{self.format_width}x}")
+                    break
+            return 1
+                    
+    def check_pkt(self):
+        if len(self.data) == 0:
+            assert False, "[ERROR] Packet is not generated."
+    
+    def generate(self, pkt_size = None, pkt_size_type = 'random', pattern = 'random', delay_type = 'short', delay = None):
+        # If packet size is not specified, then
+        # use random value
+        pkt_size_in_words = self.gen_pkt_size(pkt_size, pkt_size_type)
+        # If delay is not set in cycles
+        # then use range of delay
+        if delay is not None:
+            self.delay = delay
+        else:
+            if delay_type == 'no_delay':
+                self.delay = 0
+            elif delay_type == 'short':
+                self.delay = random.randint(1,5)
+            elif delay_type == 'medium':
+                self.delay = random.randint(5,10)
+            elif delay_type == 'long':
+                self.delay = random.randint(11,50)
+            else:
+                self.delay = random.randint(51,100)
+        # Generate data using pattern
+        if(pattern == 'increment'):
+            for word_indx in range(pkt_size_in_words):
+                word = word_indx % 8*self.word_size
+                self.data.append(word)
+        else:
+            print("[WARNING] :none of the known patterns are used. \'random\' is choosen.")
+            for word_indx in range(pkt_size_in_words):
+                word = random.randint(0, 2**(8*self.word_size))
+                self.data.append(word)
+
+    #---------------------------------
+    # Generate pkt_size.
+    #---------------------------------
+    
+    def gen_pkt_size(self, pkt_size, pkt_size_type):
+        # If pkt_size was not set
+        if pkt_size is None:
+            print(f"pkt_size_type {pkt_size_type}")
+            if pkt_size_type == 'random':
+                pkt_size_type = random.choice(['small', 'medium', 'long'])
+            print(f"pkt_size_type {pkt_size_type}")
+            # Randomize pkt_size
+            if pkt_size_type == 'one_word':
+                pkt_size = random.randint(1,self.word_size)            
+            elif pkt_size_type == 'small':
+                pkt_size = random.randint(self.word_size+1, self.word_size*5)
+            elif pkt_size_type == 'medium':
+                pkt_size = random.randint(self.word_size*5, self.word_size*50)
+            elif pkt_size_type == 'long':
+                pkt_size = random.randint(self.word_size*50, self.word_size*150)
+            else:
+                raise ValueError("[ERROR] Invalid pkt_size_type: " + str(pkt_size_type))
+        return pkt_size
+
+    def print_pkt(self):
+        word_indx = 0
+        dbg = ''
+        dbg = dbg + f"\t WORD_SIZE: {self.word_size}\n"
+        dbg = dbg + f"\t PKT_SIZE : {len(self.data)}\n\n"
+        for word_indx in range (len(self.data)):
+            if word_indx % Packet.dbg_words_in_line == 0:
+                dbg = dbg + "\n"
+                dbg = dbg + f"0x{word_indx:{Packet.dbg_fill}{Packet.dbg_width}x}: "
+            dbg = dbg + f" 0x{self.data[word_indx]:{Packet.dbg_fill}{self.format_width}x} "
+            word_indx += 1
+
+        dbg = dbg + "\n"
+        print(dbg)
