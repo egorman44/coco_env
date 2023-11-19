1b89b72eb5d8b775dce42dc810a9965decff0390
egorman44 <egorushka44@gmail.com>
2023-11-19 16:17:54 +0400

    [BIN_OPERATION] Add get_byte_list and get_word_list functions to convert list of words into list of bytes back and forth.

--- bin_operation.py
+++ bin_operation.py
@@ -4,3 +4,25 @@ def countones(n):
         count += n & 1
         n = n >> 1
     return count
+
+def get_byte_list(word_list, word_width, bytes_num):
+    byte_list = []
+    word_indx = 0
+    for byte_indx in range (bytes_num):
+        byte_list.append((word_list[word_indx] >> (byte_indx % word_width)*8) & 0xFF)
+        if (byte_indx % word_width) == word_width-1:
+            word_indx += 1
+    return byte_list            
+
+def get_word_list(byte_list, word_width, bytes_num):
+    word_list = []
+    word_indx = 0
+    word      = 0
+    for byte_indx in range (bytes_num):
+        word = (byte_list[byte_indx] << (byte_indx % word_width)*8) | word
+        if (byte_indx % word_width) == word_width-1 or byte_indx == bytes_num-1:
+            word_list.append(word)
+            word = 0
+    return word_list
+
+            
