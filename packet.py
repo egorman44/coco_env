edd0534c377c4d3d5a1e279d2aa22feb4d5edb16
egorman44 <easmirnov44@gmail.com>
2024-01-31 23:14:05 +0400

    [PKT] Rewrite packet.data to be a byte list.Delete word_size.

--- packet.py
+++ packet.py
@@ -1,25 +1,21 @@
-# TODO: Rewrite packet. data should be a byte list, not a word list.
-
 import random
 import math
 class Packet:
 
-    dbg_wordsыру_in_line = 8
+    dbg_words_in_line = 8
     dbg_fill = '0'
     dbg_width = 3
     
     # Class members:
     #   self.data
-    #   self.word_size
     #   self.delay
     
-    def __init__(self, name, word_size = 1):
+    def __init__(self, name, format_width=1):
         self.name         = name
-        self.word_size    = word_size
         self.data         = []
         self.pkt_size     = None
         self.delay        = None
-        self.format_width = int(self.word_size)*2
+        self.format_width = format_width
 
     def copy(self, ref_pkt):
         self.data = ref_pkt.data.copy()
@@ -67,13 +63,13 @@ class Packet:
                 pkt_size_type = random.choice(['small', 'medium', 'long'])
                 # Randomize pkt_size
             if pkt_size_type == 'one_word':
-                self.pkt_size = random.randint(1,self.word_size)            
+                self.pkt_size = random.randint(1,10)            
             elif pkt_size_type == 'small':
-                self.pkt_size = random.randint(self.word_size+1, self.word_size*5)
+                self.pkt_size = random.randint(10,100)
             elif pkt_size_type == 'medium':
-                self.pkt_size = random.randint(self.word_size*5, self.word_size*50)
+                self.pkt_size = random.randint(100, 500)
             elif pkt_size_type == 'long':
-                self.pkt_size = random.randint(self.word_size*50, self.word_size*150)
+                self.pkt_size = random.randint(500, 1500)
             else:
                 raise ValueError("[ERROR] Invalid pkt_size_type: " + str(pkt_size_type))
             
@@ -105,55 +101,49 @@ class Packet:
             self.pkt_size = ref_pkt.pkt_size
         else:
             # Calculates words number and valid bytes in the last cycle of the transaction
-            pkt_size_in_words = math.ceil(self.pkt_size / self.word_size)
-            last_word_bytes_valid = self.pkt_size % self.word_size
+            pkt_size_in_words = math.ceil(self.pkt_size / self.format_width)
+            last_word_bytes_valid = self.pkt_size % self.format_width
             if(pattern == 'increment'):
                 for word_indx in range(pkt_size_in_words):
-                    word = word_indx % 8*self.word_size
+                    word = word_indx % 8*self.format_width
                     self.data.append(word)
             else:
                 print("[WARNING] :none of the known patterns are used. \'random\' is choosen.")
-                for word_indx in range(pkt_size_in_words):
-                    # Check if bytes number is not word aligned in the last cycle:
-                    if word_indx == pkt_size_in_words-1 and last_word_bytes_valid:
-                        word = random.randint(0, 2**(8*last_word_bytes_valid))
-                    else:
-                        word = random.randint(0, 2**(8*self.word_size))
-                    self.data.append(word)
-
+                for indx in range(self.pkt_size):
+                    self.data.append(random.randint(0, 2**8-1))
+                    
     #---------------------------------
     # Convert data into the byte list
     #---------------------------------
     
-    def get_byte_list(self):
-        byte_list = []
+    def get_word_list(self, word_size):
+        word_list = []
+        word = 0        
+        for i in range(self.pkt_size):
+            word = word | (self.data[i] << (i % word_size)*8)
+            if (i % word_size == word_size-1) or (i == len(self.data)-1):                
+                word_list.append(word)
+                word = 0
+        return word_list
+
+    def write_word_list(self, word_list, pkt_size, word_size):
+        self.data = []
         word_cntr = 0
         byte_cntr = 0
-        for i in range (self.pkt_size):
-            byte_cntr = i % self.word_size
-            byte = (self.data[word_cntr] >> (byte_cntr * 8)) & 0xFF
-            byte_list.append(byte)
-            if(byte_cntr) == self.word_size - 1:
+        self.pkt_size = pkt_size
+        for i in range(pkt_size):
+            byte_cntr = i % word_size
+            byte = (word_list[word_cntr] >> (byte_cntr * 8)) & 0xFF
+            self.data.append(byte)
+            if(byte_cntr) == word_size - 1:
                 word_cntr += 1
-        return byte_list
-
-    def write_byte_list(self, byte_list):        
-        self.data = []
-        self.pkt_size = len(byte_list)
-        word = 0        
-        for i in range(len(byte_list)):
-            word = word | (byte_list[i] << (i % self.word_size)*8)
-            if (i % self.word_size == self.word_size-1) or (i == len(byte_list)-1):                
-                self.data.append(word)
-                word = 0
-
+                
     def write_number(self, val, width_in_bits):
         byte_list = []
         self.data = []
         self.pkt_size = math.ceil(width_in_bits/8)
         for i in range(self.pkt_size):
-            byte_list.append((val >> i*8) & 0xFF)
-        self.write_byte_list(byte_list)        
+            self.data.append((val >> i*8) & 0xFF)
 
     #---------------------------------
     # Corrupt data list
@@ -163,10 +153,8 @@ class Packet:
         corrupt_words = random.sample(range(0,len(self.data)), words_num)
         print(f"corrupt_words {corrupt_words}")
         for word in corrupt_words:
-            bit_position = random.randint(0, (self.word_size*8)-1)
-            self.data[word] = self.data[word] ^ (1 << bit_position)
-        
-        
+            bit_position = random.randint(0, 7)
+            self.data[word] = self.data[word] ^ (1 << bit_position)        
                 
     #---------------------------------
     # Print packet
@@ -176,17 +164,18 @@ class Packet:
         dbg = ''
         if source:            
             dbg = source + '\n'
-        dbg = dbg + f"\t PKT_NAME : {self.name} \n"
-        dbg = dbg + f"\t WORD_SIZE: {self.word_size}\n"
-        dbg = dbg + f"\t PKT_SIZE : {self.pkt_size}\n"
+        dbg = dbg + f"\t PKT_NAME    : {self.name} \n"
+        dbg = dbg + f"\t FORMAT_WIDTH: {self.format_width}\n"
+        dbg = dbg + f"\t PKT_SIZE    : {self.pkt_size}\n"
         if self.delay is not None:
-            dbg = dbg + f"\t DELAY    : {self.delay}\n"
-        dbg = dbg + f"\t DATA     : \n"
+            dbg = dbg + f"\t DELAY       : {self.delay}\n"
+        dbg = dbg + f"\t DATA        : \n"
         for word_indx in range (len(self.data)):
             if word_indx % Packet.dbg_words_in_line == 0:
                 dbg = dbg + "\n"
                 dbg = dbg + f"\t\t{word_indx:{Packet.dbg_fill}{Packet.dbg_width}d}: "
-            dbg = dbg + f" 0x{self.data[word_indx]:{Packet.dbg_fill}{self.format_width}x} "
+            #dbg = dbg + f" 0x{self.data[word_indx]:{Packet.dbg_fill}{self.format_width}x} "
+            dbg = dbg + f" 0x{self.data[word_indx]:02x} "
             word_indx += 1
 
         dbg = dbg + "\n"
